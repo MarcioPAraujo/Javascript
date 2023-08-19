5d75f04152dc1e9fed5c9bd5c2e8f81f233050d8
mapa <marciopereiraaraujo1997d1@gmail.com>
2023-08-19 11:49:51 -0300

    variables and constants

---
+++ code-2/app.js
@@ -0,0 +1,10 @@
+//alert('hello and welcome');
+const myBirthday = 2001;
+let currentYear = 2023;
+const tenYears = 10;
+const myAge = currentYear - myBirthday;
+
+console.log(myAge);
+
+
+console.log(`in ${tenYears} years, I'll be ${myAge+tenYears} `);
