0c3a2ce3ef1dec99aa227828ce4d1b74e312a4e8
mapa <marciopereiraaraujo1997d1@gmail.com>
2023-08-26 10:35:36 -0300

    create category service concluded

--- college_project/src/controller/category/CreateCategoryController.ts
+++ college_project/src/controller/category/CreateCategoryController.ts
@@ -1,11 +1,16 @@
 import {Request, Response} from "express";
+import { CreateCategoryService } from "../../service/category/CreateCategoryService";
+
  class CreateCategoryController{
+
     async handle(request:Request, response:Response){
+        
         const {name, area} = request.body;
-        const category={
-            name:name,
-            area:area
-        };
+
+        const createCategoryService = new CreateCategoryService();
+
+        const category = await createCategoryService.execute({name,area});
+
         return response.json(category);
     }
  }
