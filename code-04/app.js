848db71bd48162f7957345c9c1e0b20f405cf75c
mapa <marciopereiraaraujo1997d1@gmail.com>
2023-09-11 11:21:10 -0300

    exercices concluded

--- code-04/app.js
+++ code-04/app.js
@@ -13,9 +13,24 @@ console.log(info)
 const maleDogsName = ['bambo','dorô']
 const femaleDogsName = ['lora','gota']
 
-let DogsName = maleDogsName.concat(femaleDogsName)
-console.log(DogsName)
+let dogsName = maleDogsName.concat(femaleDogsName)
+console.log(dogsName)
 
 
-DogsName.pop()
-console.log(DogsName[DogsName.length-1])
+dogsName.pop()
+console.log(dogsName[dogsName.length-1])
+
+dogsName.push('nina')
+
+const initial = `${dogsName[0][0]}, ${dogsName[1][0]}, ${dogsName[2][0]}, ${dogsName[3][0]}`
+
+console.log(initial)
+console.log(initial.toUpperCase())
+
+const candy = dogsName[0][0]+dogsName[0][1]+dogsName[1][0]+dogsName[1][1]+dogsName[2][0]+dogsName[2][1]
+console.log(candy)
+
+console.log(`${candy} é uma palavra inventada`)
+const oddNumbers = [3,5,7]
+oddNumbers[1] = oddNumbers[1]+4
+console.log(oddNumbers[1]**3)
