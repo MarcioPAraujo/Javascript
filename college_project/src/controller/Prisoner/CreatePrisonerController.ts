59ff8402ad958a4b1de2f397bdc4e6c8048c930d
mapa <marciopereiraaraujo1997d1@gmail.com>
2023-09-30 10:29:31 -0300

    prisoner controler is done

---
+++ college_project/src/controller/Prisoner/CreatePrisonerController.ts
@@ -0,0 +1,22 @@
+import { Request,Response } from "express";
+import { CreatePrisonerService } from "../../service/Prisoner/CreatePrisonerService";
+
+
+class CreatePrisonerController{
+    async handle(request:Request,response:Response){
+        const {name,height,weight,age,nationality} = request.body
+
+        const prisoner={
+            name:name,
+            height:height,
+            weight:weight,
+            age:age,
+            nationality:nationality
+        }
+
+        const createPrisonerService = new CreatePrisonerService()
+        const retrieve = await createPrisonerService.execute(prisoner)
+        return response.json(retrieve)
+    }
+}
+export{CreatePrisonerController}
