39e54b398f8deb748e43f00097583fa4e6ecbd5f
mapa <marciopereiraaraujo1997d1@gmail.com>
2023-11-25 21:31:00 -0300

    added cors

--- college_project/backend_project/src/server.ts
+++ college_project/backend_project/src/server.ts
@@ -2,10 +2,11 @@ import express,{Request,Response,NextFunction} from "express";
 import "express-async-errors"
 import "reflect-metadata";
 import "./database/Index";
-
+const cors = require('cors');
 import { router } from "./routes";
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.use(
      (err: Error, request: Request, response: Response, next: NextFunction) => {
